2e1f2db794835c9408ddb5d9af1dc0d8e94e47e6
xrhdevil <xrhdevil@126.com>
2026-08-25 10:31:53 +0800

    第一次上传到远程仓库 版本V1.0

---
+++ app/src/main/java/com/example/agapkcicd/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.agapkcicd;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
